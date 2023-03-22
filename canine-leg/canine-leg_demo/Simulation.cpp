457009d6daed9d5920717b84ccc6b9a941a3cf58
Jaehoon-An <dkswogns46@gmail.com>
2023-03-22 19:42:37 +0900

    Added time checker in Simulation.cpp

--- canine-leg/canine-leg_demo/Simulation.cpp
+++ canine-leg/canine-leg_demo/Simulation.cpp
@@ -15,13 +15,22 @@ TorchModelManager torchManager;
 void* NRTTorchThread(void* arg)
 {
     std::cout << "entered #nrt_torch_thread" << std::endl;
+    struct timespec TIME_1;
+    struct timespec TIME_2;
+
     while (true)
     {
+        clock_gettime(CLOCK_REALTIME, &TIME_1);
         torchManager.torchFunction();
+        clock_gettime(CLOCK_REALTIME, &TIME_2);
+
+        /**
+         * It takes about 70 ~ 311 micro seconds. / mean : 120 micro seconds
+         */
+//        std::cout << "one estimation time : "<<timediff_us(&TIME_1, &TIME_2) << std::endl;
         usleep(TORCH_dT * 1e6);
     }
 }
-
 
 int main(int argc, char* argv[])
 {
